0bce6148aa31f8076cd37eca805c66173ebfcd30
Kentaro Kawamoto <kentaro.kawamoto@gmail.com>
2011-09-11 10:17:24 +0900

    completed calculator example.

--- 3/chapter8.hs
+++ 3/chapter8.hs
@@ -1,31 +1,137 @@
+module Parsing where
 
-type Parser a = String -> [(a, String)]
+import Char
+import Monad
 
-return :: a -> Parser a
-return v = \inp -> [(v, inp)]
+infixr 5 +++
+
+newtype Parser a = P (String -> [(a, String)])
+
+instance Monad Parser where
+	return v = P (\inp -> [(v, inp)])
+	p >>= f = P (\inp -> case parse p inp of
+			[] -> []
+			[(v, out)] -> parse (f v) out)
+
+instance MonadPlus Parser where
+	mzero	= P (\inp -> [])
+	p `mplus` q = P (\inp -> case parse p inp of
+			[] 		-> parse q inp
+			[(v, out)] 	-> [(v, out)])
 
 failure :: Parser a
-failure = \inp -> []
+failure = mzero
 
 item :: Parser Char
-item = \inp -> case inp of
+item = P (\inp -> case inp of
 		[] -> []
-		(x:xs) -> [(x, xs)]
+		(x:xs) -> [(x, xs)])
 
 parse :: Parser a -> String -> [(a, String)]
-parse p inp = p inp
+parse (P p) inp = p inp
 
-(>==) :: Parser a -> (a -> Parser b) -> Parser b
-p >== f = \inp -> case parse p inp of
-			[] -> []
-			[(v, out)] -> parse (f v) out
+(+++)	:: Parser a -> Parser a -> Parser a
+p +++ q	= p `mplus` q
 
-p :: Parser (Char, Char)
-p = do
+p1 :: Parser (Char, Char)
+p1 = do
 	x <- item
 	item
 	y <- item
-	Main.return (x, y)
+	return (x, y)
  
+sat	:: (Char -> Bool) -> Parser Char
+sat p	= do
+	x <- item
+	if p x then return x else failure
 
+digit	= sat isDigit
+lower	= sat isLower
+upper	= sat isUpper
+letter	= sat isAlpha
+alphanum = sat isAlphaNum
+char x	= sat (== x)
+
+string		:: String -> Parser String
+string []	= return []
+string (x:xs)	= do
+	char x
+	string xs
+	return (x:xs)
+
+many	:: Parser a -> Parser [a]
+many p	= many1 p +++ return []
+many1	:: Parser a -> Parser [a]
+many1 p	= do v <- p
+    	     vs <- many p
+	     return (v:vs)
+
+ident	:: Parser String
+ident	= do
+	x <- lower
+	xs <- many alphanum
+	return (x:xs)
+
+nat	:: Parser Int
+nat	= do
+	xs <- many1 digit
+	return (read xs)
+
+space	:: Parser ()
+space	= do
+	many (sat isSpace)
+	return ()
+
+token	:: Parser a -> Parser a
+token p	= do
+	space
+	v <- p
+	space
+	return v
+
+identifier	= token ident
+natural		= token nat
+symbol xs	= token (string xs)
+
+p2 :: Parser [Int]
+p2 = do
+	symbol "["
+	n <- natural
+	ns <- many (do
+		symbol ","
+		natural)
+	symbol "]"
+	return (n:ns)
+
+expr	:: Parser Int
+expr	= do
+	t <- term
+	do
+		symbol "+"
+		e <- expr
+		return (t + e)
+		+++ return t
+
+term	:: Parser Int
+term	= do
+	f <- factor
+	do
+		symbol "*"
+		t <- term
+		return (f * t)
+		+++ return f
+
+factor	:: Parser Int
+factor	= do
+	symbol "("
+	e <- expr
+	symbol ")"
+	return e
+	+++ natural
+
+eval	:: String -> Int
+eval xs	= case parse expr xs of
+	[(n, [])] -> n
+	[(_, out)] -> error("unused input " ++ out)
+	[] -> error "invalid input"	
 
